5e3beddf1c50601bef4f3753431d5c121fd6382c
keitey <keiteybarros@gmail.com>
2022-07-15 14:01:28 -0300

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+import "./Responsive.sass"
+
 const App = () => {
   return (
     <div className="container-fluid" style={{ padding: "0px" }}>
